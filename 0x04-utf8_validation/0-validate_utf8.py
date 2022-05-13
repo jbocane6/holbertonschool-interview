4c7e10f344555f81d11a9af384c3715f80c51c4a
Juan Bocanegra <jbocane6@gmail.com>
2022-05-12 23:27:37 -0500

    [UPDATE] file 0 v.7

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,6 +15,30 @@ def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding.
     """
+    nbrOfbytes = 0
+    for i in data:
+        mask = 1 << 7
+        if nbrOfbytes == 0:
+            while mask & i:
+                nbrOfbytes += 1
+                mask >>= 1
+            if nbrOfbytes == 0:
+                continue
+            elif nbrOfbytes == 1 or nbrOfbytes > 4:
+                return False
+        else:
+            if not (i & 1 << 7 and not (i & 1 << 6)):
+                return False
+        nbrOfbytes = nbrOfbytes - 1
+    if nbrOfbytes == 0:
+        return True
+    return False
+
+def validUtf8(self, data):
+        """
+        :type data: List[int]
+        :rtype: bool
+        """
     count = 0
     for c in data:
         if count == 0:
@@ -29,5 +53,5 @@ def validUTF8(data):
         else:
             if (c >> 6) != 0b10:
                 return False
-            count -= 1
+        count -= 1
     return count == 0
