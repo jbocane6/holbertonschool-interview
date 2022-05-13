037b18275683de3f830d3b18d207890f2a78a39a
Juan Bocanegra <jbocane6@gmail.com>
2022-05-12 23:18:59 -0500

    [UPDATE] file 0 v.7

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,22 +15,19 @@ def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding.
     """
-    data = iter(data)
-    for leading_byte in data:
-        leading_ones = _count_leading_ones(leading_byte)
-        if leading_ones in [1, 7, 8]:
-            return False
-        for _ in range(leading_ones - 1):
-            trailing_byte = next(data, None)
-            if trailing_byte is None or trailing_byte >> 6 != 0b10:
+    count = 0
+    for c in data:
+        if count == 0:
+            if (c >> 5) == 0b110:
+                count = 1
+            elif (c >> 4) == 0b1110:
+                count = 2
+            elif (c >> 3) == 0b11110:
+                count = 3
+            elif (c >> 7):
                 return False
-    return True
-
-
-def _count_leading_ones(byte):
-    """Counts the leading ones."""
-
-    for i in range(8):
-        if byte >> 7 - i == 0b11111111 >> 7 - i & ~1:
-            return i
-    return 8
+        else:
+            if (c >> 6) != 0b10:
+                return False
+            count -= 1
+    return count == 0
