efb41b7c972d826e388c2bbc751e7630ed33d091
Juan Camilo Bocanegra Osorio <75183250+jbocane6@users.noreply.github.com>
2023-12-14 09:54:25 -0500

    Update 0-prime_game.py

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -17,120 +17,28 @@ determine who the winner of each game is.
 """
 
 
-def isWinner(x,nums):
-  """prueba"""
-  Count_Maria = 0
-  Count_Ben = 0
-
- 
-  for i in nums:
-      if i == 1:
-        Count_Ben = Count_Ben + 1
-      if i == 2:
-        Count_Maria = Count_Maria + 1   
-      if i != 1 and i != 2 and type(i) == int: 
-        if i is float:
-            i = round(i)   
-        vector = list(range(1,i+1))
-        vector_de_primos = obtener_numeros_primos(i)
-        while True:
-
-            fin = analizar_si_quedan_movimientos(vector)
-            if fin:
-
-                while True:
-                    
-                    for elemento_vector in vector:
-                        if elemento_vector in vector_de_primos:
-                            Maria = elemento_vector
-                            break
-                    if Maria in vector_de_primos:
-                        break
-                vector = eliminar_elementos(vector,Maria)
-                fin = analizar_si_quedan_movimientos(vector)
-                if fin:
-                    while True:
-                        for elemento_vector in vector:
-                            if elemento_vector in vector_de_primos:
-                                Ben = elemento_vector
-                                break
-
-                        if Ben in vector_de_primos:
-                            break
-                    
-                    vector = eliminar_elementos(vector,Ben)
-            
-                    fin = analizar_si_quedan_movimientos(vector)
-                else:
-                    Count_Maria = Count_Maria+1
-                    break
-            else:
-                Count_Ben = Count_Ben + 1
-                break
-
-  if Count_Ben > Count_Maria:
-    winner = 'Ben'
-  if Count_Ben < Count_Maria:
-    winner = 'Maria'
-  if Count_Ben == Count_Maria:
-    winner = 'None'
-
-
-  return winner
-
-def Verificar_primo_y_existencia(numero):
-    """prueba"""
-    if numero == 2:  
-        indicador2 = True
-    else:    
-        if numero < 2:  
-            indicador2 = False
-        else:
-            
-            for test in list(range(2,numero)): 
-                
-                if numero % test == 0:
-                    indicador2 = False
-                    break
-                else:
-                    indicador2 = True
-
-    
-    if  indicador2:
-        return True
-    else:
-        return False
-
-def analizar_si_quedan_movimientos(vector):
-    """prueba"""
-    for i in vector:
-        if Verificar_primo_y_existencia(i):
-            movimientos = True
-            break
-        else:
-            movimientos = False
-            
-    return movimientos
-
-def eliminar_elementos(vector,numero):
-
-    vector.remove(numero)
-    for i in vector:
-        if i % numero == 0:
-            vector.remove(i)
-
-    return vector
-
-def obtener_numeros_primos(n):
-  """prueba"""
-  if n > 2:
-    vector = list(range(2,n+1))
-    numero=0
-    while True:
-        primo = vector[numero]
-        vector = [ni for ni in vector if (ni % primo != 0)]  
-        vector.insert(numero,primo)
-        if primo*2 >= n:
-            break
-        numero = numero+1
-    return vector
+def isWinner(x, nums):
+    """Prime game"""
+    if x < 1 or not nums:
+        return None
+    n = max(nums)
+    primes = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not primes[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            primes[j] = False
+    primes[0] = primes[1] = False
+    c = 0
+    for i in range(len(primes)):
+        if primes[i]:
+            c += 1
+        primes[i] = c
+    turns = 0
+    for n in nums:
+        turns += primes[n] % 2 == 1
+    if turns * 2 == len(nums):
+        return None
+    if turns * 2 > len(nums):
+        return "Maria"
+    return "Ben"
